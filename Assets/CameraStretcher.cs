812a7a8670ea7c2046180fd2e96c77bb16dca41a
Federico Paolini <fp@nuxi.it>
2025-11-12 17:16:34 +0100

    Added movement and camera dampening

--- Assets/CameraStretcher.cs
+++ Assets/CameraStretcher.cs
@@ -1,16 +1,53 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class CameraStretcher : MonoBehaviour
 {
+    public CinemachineCamera followCamera;
+    public CinemachineThirdPersonFollow followArm;
+    private Rigidbody body;
+
+    private float baseSpeed;
+    private float targetSpeed;
+    private float currentSpeed;
+    public float cameraAccelCap;
+    private float cameraAcceleration;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        body = GetComponent<Rigidbody>();
+        baseSpeed = followCamera.Lens.FieldOfView;
+        currentSpeed = baseSpeed;
+        cameraAcceleration = cameraAccelCap / 20;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        targetSpeed = baseSpeed + new Vector2(body.linearVelocity.x, body.linearVelocity.z).magnitude;
+        if (currentSpeed < targetSpeed)
+        {
+            if (currentSpeed + cameraAcceleration >= targetSpeed)
+            {
+                currentSpeed = targetSpeed;
+            }
+            else
+            {
+                currentSpeed += cameraAcceleration;
+            }
+        }
+        else if (currentSpeed > targetSpeed)
+        {
+            if (currentSpeed - cameraAcceleration <= targetSpeed)
+            {
+                currentSpeed = targetSpeed;
+            }
+            else
+            {
+                currentSpeed -= cameraAcceleration;
+            }
+        }
+        followCamera.Lens.FieldOfView = currentSpeed;
+        followArm.CameraDistance = currentSpeed/10;
     }
 }
--- Assets/WindPush.cs
+++ Assets/WindPush.cs
@@ -5,6 +5,8 @@ public class WindPush : MonoBehaviour
     public float windStrength;
     public float windSpeed;
     private Rigidbody body;
+    private Vector2 horizontalVelocity;
+    private float velocityDampening;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,9 +17,19 @@ public class WindPush : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (body.linearVelocity.magnitude < windSpeed)
+        horizontalVelocity = new Vector2(body.linearVelocity.x, body.linearVelocity.z);
+        if (horizontalVelocity.magnitude < windSpeed)
         {
-            body.AddForce(new Vector3(windStrength, 0.0f, windStrength));
+            if (horizontalVelocity.magnitude > 1)
+            {
+                velocityDampening = Mathf.Sqrt((windSpeed - horizontalVelocity.magnitude)/windSpeed);
+                body.AddForce(new Vector3(windStrength*velocityDampening, 0f, windStrength*velocityDampening));
+            }
+            else
+            {
+                body.AddForce(new Vector3(windStrength, 0.0f, windStrength));
+            }
+
         }
     }
 }
