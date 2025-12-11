e69ebd08593bcc61959c69ba50db07d39850e334
Federico Paolini <fp@nuxi.it>
2025-12-11 12:45:35 +0100

    Added jump logic, physics still missing

--- Assets/General/Scripts/FeralColonistNav.cs
+++ Assets/General/Scripts/FeralColonistNav.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -6,11 +7,17 @@ public class FeralColonistNav : MonoBehaviour
     public PlayerInput player;
     public GameObject body;
     private NavMeshAgent navigation;
-    private FeralColonistBehavior currentBehavior = FeralColonistBehavior.Idle;
+    private float playerDistance;
+    private Rigidbody jumpRB;
+    public FeralColonistBehavior currentBehavior = FeralColonistBehavior.Idle;
 
     [Header("AI variables")]
     public float aggroRange;
     public float losAggroRange;
+    public float jumpRange;
+    public float jumpStrength;
+    public float jumpChargeTime;
+    public float meleeAttackRange;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -30,13 +37,45 @@ public class FeralColonistNav : MonoBehaviour
 
     private void BehaviorSwitchCheck()
     {
+        playerDistance = (player.transform.position - body.transform.position).magnitude;
+        //Fai passare il feral colonist da idle a closing
         if (currentBehavior == FeralColonistBehavior.Idle)
         {
-            if ((CheckLOS() && (player.transform.position - body.transform.position).magnitude <= losAggroRange) || (player.transform.position - body.transform.position).magnitude <= aggroRange)
+            if ((CheckLOS() && playerDistance <= losAggroRange) || playerDistance <= aggroRange)
             {
                 currentBehavior = FeralColonistBehavior.Closing;
             }
         }
+        //Fai passare il feral colonist da closing a jumping
+        //First check = il giocatore deve essere irraggiungibile con attacco melee base e dentro il jump range
+        if (currentBehavior == FeralColonistBehavior.Closing){
+            if ((player.height > meleeAttackRange+1) && playerDistance <= jumpRange)
+            {
+                //Second check = la distanza del giocatore dal terreno è maggiore della distanza del feral colonist dal punto proiettato (aka l'angolo del salto è >45)
+                if (player.height > playerDistance){
+                    currentBehavior = FeralColonistBehavior.Jumping;
+                    StartCoroutine(Jump());
+                }
+            }
+
+                //Fai passare il feral colonist da closing ad attacking
+            //if ((player.transform.position - body.transform.position).magnitude <= meleeAttackRange)
+            //{
+            //
+            //}
+        }
+    }
+
+    private IEnumerator Jump()
+    {
+        float timer = 0;
+        while (timer < jumpChargeTime)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        //Effettua il jump fisico calcolando la traiettoria.
+
     }
 
     private bool CheckLOS()
@@ -48,12 +87,12 @@ public class FeralColonistNav : MonoBehaviour
         }
         else return false;
     }
-
     public enum FeralColonistBehavior
     {
         Idle,
         Closing,
         Attacking,
+        Jumping,
         Escaping
     }
 }
