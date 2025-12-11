eaa5f62467d590f7b37bb84ba10234f1413eda48
Federico Paolini <fp@nuxi.it>
2025-12-11 11:49:07 +0100

    Added basic aggro range logic to feral colonist

--- Assets/General/Scripts/FeralColonistNav.cs
+++ Assets/General/Scripts/FeralColonistNav.cs
@@ -4,7 +4,14 @@ using UnityEngine.AI;
 public class FeralColonistNav : MonoBehaviour
 {
     public PlayerInput player;
+    public GameObject body;
     private NavMeshAgent navigation;
+    private FeralColonistBehavior currentBehavior = FeralColonistBehavior.Idle;
+
+    [Header("AI variables")]
+    public float aggroRange;
+    public float losAggroRange;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,7 +21,32 @@ public class FeralColonistNav : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        navigation.SetDestination(player.projectedPosition);
+        BehaviorSwitchCheck();
+        if (currentBehavior == FeralColonistBehavior.Closing)
+        {
+            navigation.SetDestination(player.projectedPosition);
+        }
+    }
+
+    private void BehaviorSwitchCheck()
+    {
+        if (currentBehavior == FeralColonistBehavior.Idle)
+        {
+            if ((CheckLOS() && (player.transform.position - body.transform.position).magnitude <= losAggroRange) || (player.transform.position - body.transform.position).magnitude <= aggroRange)
+            {
+                currentBehavior = FeralColonistBehavior.Closing;
+            }
+        }
+    }
+
+    private bool CheckLOS()
+    {
+        //Se vengono creati nuovi layer di terrain, cambiare la layermask del raycast
+        if (!Physics.Raycast(body.transform.position, player.transform.position - body.transform.position, (player.transform.position - body.transform.position).magnitude, LayerMask.GetMask("Default")))
+        {
+            return true;
+        }
+        else return false;
     }
 
     public enum FeralColonistBehavior
