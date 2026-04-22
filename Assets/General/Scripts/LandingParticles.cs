5aa5a3f88bb181b2515c40e8c501af211825fb7a
Osiarna <andrea.pistocchi2004@gmail.com>
2026-04-22 19:10:39 +0200

    Fix projectiles and particles

--- Assets/General/Scripts/LandingParticles.cs
+++ Assets/General/Scripts/LandingParticles.cs
@@ -2,22 +2,47 @@ using UnityEngine;
 
 public class LandingParticles : MonoBehaviour
 {
-    public ParticleSystem Dust;
+    public ParticleSystem dustPrefab;
     public PlayerInput movement;
-    public bool count;
+
+    public float minFallHeight = 2f;
+
+    private bool wasGrounded;
+    private float highestY;
+
     void Update()
     {
-        if (movement.grounded)
+        bool isGrounded = movement.grounded;
+        float currentY = movement.transform.position.y;
+
+        if (!isGrounded)
         {
-            Dust.Play();
-            count = true;
+            if (currentY > highestY)
+                highestY = currentY;
         }
 
-        if (count = true)
+        if (!wasGrounded && isGrounded)
         {
-            Dust.Stop();
+            float fallDistance = highestY - currentY;
+
+            if (fallDistance > minFallHeight)
+            {
+                SpawnDust();
+            }
+
+            highestY = currentY;
         }
+
+        wasGrounded = isGrounded;
     }
 
-    
+    void SpawnDust()
+    {
+        Vector3 spawnPos = movement.transform.position;
+
+        // leggermente sotto i piedi
+        spawnPos += Vector3.down * .9f;
+
+        Instantiate(dustPrefab, spawnPos, Quaternion.identity);
+    }
 }
