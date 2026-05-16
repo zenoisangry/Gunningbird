8a43c31dec58bdb3a2ac6d53bd5876b02136d252
Federico Paolini <fp@nuxi.it>
2026-05-16 14:22:45 +0200

    Fully fixed rotation to not be impacted by collisions

--- Assets/General/Scripts/Enemies/FeralColonistMovement.cs
+++ Assets/General/Scripts/Enemies/FeralColonistMovement.cs
@@ -41,8 +41,10 @@ public class FeralColonistMovement : MonoBehaviour
     {
         if (target != prevTarget)
         {
+            rb.freezeRotation = false;
             rb.MoveRotation(Quaternion.LookRotation(new Vector3(target.x, 0, target.z) - new Vector3(transform.position.x, 0, transform.position.z)) * Quaternion.Euler(Vector3.up * 90));
             prevTarget = target;
+            rb.freezeRotation = true;
         }
     }
     private void FollowNavAgent()
